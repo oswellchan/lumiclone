cf449554cfbc7a83a410b44d1494394666fe08ef
Oswell Chan <oswellchan@gmail.com>
2018-08-31 01:11:16 +0800

    lumiclone release v0.1

--- app/components/UI/Constants.js
+++
@@ -1,4 +0,0 @@
-export const Orange = '#eb9634';
-export const DarkOrange = '#c4732a';
-export const OffWhite = '#ebe8ed';
-export const DarkOffWhite = '#c4c2c6';
--- app/index.js
+++
@@ -1,5 +0,0 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
-import { App } from './components/App';
-
-ReactDOM.render(<App />, document.getElementById('app'));
